bbd24652a3b08e873a9ae35d06ee3b5004b463a4
ziokass01 <121779576+ziokass01@users.noreply.github.com>
2026-03-24 15:29:24 +0700

    Add files via upload

--- src/pages/ResetKey.tsx
+++ src/pages/ResetKey.tsx
@@ -148,7 +148,10 @@ export function ResetKeyPage() {
         <CardContent className="space-y-4">
           <Input
             value={key}
-            onChange={(e) => setKey(e.target.value.toUpperCase())}
+            onChange={(e) => {
+              setKey(e.target.value.toUpperCase());
+              setResult(null);
+            }}
             placeholder="SUNNY-XXXX-XXXX-XXXX"
             className="font-mono"
           />
